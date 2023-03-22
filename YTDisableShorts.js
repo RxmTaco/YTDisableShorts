b2c63eb849dd4d862d2bf54d718c9ec5779c48e2
Remi <117571946+RxmTaco@users.noreply.github.com>
2023-03-22 15:17:14 +0200

    Update YTDisableShorts.js

--- YTDisableShorts.js
+++ YTDisableShorts.js
@@ -5,7 +5,7 @@
 // @description  Disables YouTube Shorts Shelf on load if found.
 // @author       https://github.com/RxmTaco
 // @match        https://www.youtube.com/
-// @icon         https://github.com/RxmTaco/YTSDisableShorts/icon.png
+// @icon         https://github.com/RxmTaco/YTDisableShorts/icon.png
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
 // @grant        none
 // ==/UserScript==
