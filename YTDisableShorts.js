5fa4c47802c283d61cfd2debe3b17a046a76b12b
Remi <117571946+RxmTaco@users.noreply.github.com>
2023-03-24 00:00:11 +0200

    Update YTDisableShorts.js

--- YTDisableShorts.js
+++ YTDisableShorts.js
@@ -10,14 +10,37 @@
 // @grant        none
 // ==/UserScript==
 
+//This is a simple api used for logging how many times the YouTube Shorts Shelf has been disabled.
+//No personal information is logged, just the count of how many times it has been disabled.
+//This logging can be disabled by setting the "enableMetrics" variable to "false"
+const enableMetrics = true;
 
 $(document).ready(function() {
+    console.log("[YTDisableShorts]: Script Active");
     setInterval(remove, 1000);
 });
 
 function remove(){
     if($("#dismissible.style-scope.ytd-rich-shelf-renderer").length){
+        console.log("[YTDisableShorts]: Found Shorts Shelf");
         $("#dismissible.style-scope.ytd-rich-shelf-renderer").remove();
-        console.log("REMOVED SHORTS SHELF");
+        console.log("[YTDisableShorts]: Disabled Shorts Shelf");
+        if(enableMetrics == true) apiRequest();
     }
+}
+
+async function apiRequest(){
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'access-control-allow-origin': '*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ "action":"add" })
+    };
+    const response = await fetch('https://api.rxmtaco.gay/api/ytds', options);
+    const json = await response.json();
+    console.log("[YTDisableShorts]: YouTube Shorts has been disabled "+json.count+" times!");
+    document.cookie = json.cookie;
 }
